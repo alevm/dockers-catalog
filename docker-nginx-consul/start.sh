13463857d93295f709e0242f8b3a06408ec7be7f
an <av@levm.eu>
2015-10-28 10:10:32 +0100

    consul nginx - yet another centos adaptation (skipping dbus and init)

--- docker-nginx-consul/start.sh
+++ docker-nginx-consul/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-systemctl start nginx.service
-consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:systemctl reload nginx.service"
+/usr/sbin/nginx
+consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:/usr/sbin/nginx -s reload"
 
 
