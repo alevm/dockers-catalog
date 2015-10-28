e0044acad3e149faa66e794426d2e7f4c67c0417
an <av@levm.eu>
2015-10-28 16:49:16 +0100

    consul log level debug

--- docker-nginx-consul/start.sh
+++ docker-nginx-consul/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nginx
-consul-template -consul=$CONSUL_URL -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:nginx -s reload"
+consul-template -consul=$CONSUL_URL -log-level=debug -template="/templates/service.ctmpl:/etc/nginx/conf.d/service.conf:nginx -s reload"
 
 
